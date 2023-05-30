72b9985e0f5c74281aba74089b2007adc881822d
Jonathan Martínez Camarillo <72759898+Jonathan318042989@users.noreply.github.com>
2023-05-30 00:44:33 -0600

    Agrego consulta
    1/5

--- SQL/Query.sql
+++ SQL/Query.sql
@@ -1,22 +1,6 @@
-select nombregerente as nombreempleado
-from gerente
-where nombregerente like 'C%'
-union
-select nombrecajero
-from cajero
-where nombrecajero like 'C%'
-union
-select nombreencargado
-from encargado
-where nombreencargado like 'C%';
-
-select *
-from cliente
-where cast(fechanacimiento as char (10)) like '%-06-%';
-
-(select * from perecedero where presentacion='lata') union (select * from NOPERECEDERO where presentacion='LATA') ;
-
-(Select * from perecedero where fechacadcorta >= '2023-01-01' and fechacadcorta <= '2023-05-07' ) union (Select * from NOPERECEDERO where FECHACADLARGA >= '2023-01-01' and FECHACADLARGA <= '2023-05-07' );
-
-select *
-from cliente;
+-- El correo de los cajeros que vendieron en la primera mitad del año en la sucursal del Distrito Federal
+select idsucursal, correo, fechaventa
+from correocajero
+natural join venta
+natural join sucursal
+where estado = 'Distrito Federal' and fechaventa between '2023-01-01' and '2023-06-30';
